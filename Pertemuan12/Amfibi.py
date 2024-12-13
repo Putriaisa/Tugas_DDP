7604fd13587aad084b1c10ad2dd0bc58d1d807a3
Putriaisa <aisazahraputri@gmail.com>
2024-12-13 23:30:08 +0700

    Add files via upload

---
+++ Pertemuan12/Amfibi.py
@@ -0,0 +1,18 @@
+from Animal import Animal
+
+# setiap class child itu mempunyai 2 properti dan method
+class Amfibi(Animal):
+    def __init__(self, name, makanan, hidup, berkembang_biak, jenis_air, bernapas):
+        super().__init__(name, makanan, hidup, berkembang_biak) 
+        self.jenis_air = jenis_air
+        self.bernapas = bernapas
+
+    def info_amfibi(self):
+        super().info_animal(),
+        print("Jenis Air \t\t\t :", self.jenis_air,
+              "\nBernapas \t\t\t :", self.bernapas)
+        
+amfibi = Amfibi("Katak", "Serangga", 'Dua alam', "Bertelur", "Air Tawar", "Kulit dan Paru-paru")
+amfibi.info_amfibi()
+              
+
